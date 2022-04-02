efd864651c2d89ec77105896f80f4ca9951ecd89
LILITHl <Saosao007888@gmail.com>
2022-04-02 21:44:16 +0800

    calc avg length of reviews_sun_len

--- read.py
+++ read.py
@@ -6,4 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print('檔案讀取完畢, 總共有', len(data), '筆資料')			
+print('檔案讀取完畢, 總共有', len(data), '筆資料')	
+
+sum_len = 0 #sum_ 加總 #len  
+for d in data:
+	sum_len = sum_len + len(d)
+	
+print('留言平均為', sum_len/len(data))	
